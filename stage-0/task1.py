db164159ed35d57a188209438d6a0a08683c8aa7
valentimfilhouk <jose.valentimfilhoUK@gmail.com>
2025-02-05 16:22:05 +0000

    Create task1.py

---
+++ stage-0/task1.py
@@ -0,0 +1,13 @@
+member_0 = {'name': 'valentim', 'slackname': 'Jose Valentim', 'email':'jose.valentimfilhoUK@gmail.com' }
+member_1 = {'name': 'valentim', 'slackname': 'Jose Valentim', 'email':'jose.valentimfilhoUK@gmail.com' }
+member_2 = {'name': 'valentim', 'slackname': 'Jose Valentim', 'email':'jose.valentimfilhoUK@gmail.com' }
+member_3 = {'name': 'valentim', 'slackname': 'Jose Valentim', 'email':'jose.valentimfilhoUK@gmail.com' }
+member_4 = {'name': 'valentim', 'slackname': 'Jose Valentim', 'email':'jose.valentimfilhoUK@gmail.com' }
+
+members = []
+members.append(member_0)
+members.append(member_1)
+members.append(member_2)
+members.append(member_3)
+members.append(member_4)
+print(members)
