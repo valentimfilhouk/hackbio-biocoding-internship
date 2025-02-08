72c6c0270791ad3a018dee64266de5088eeca6c6
valentimfilhouk <jose.valentimfilhoUK@gmail.com>
2025-02-08 18:01:29 +0000

    Update task1.py

--- stage-0/task1.py
+++ stage-0/task1.py
@@ -1,8 +1,8 @@
-member_0 = {'name': 'valentim', 'slackname': 'Jose Valentim', 'email':'jose.valentimfilhoUK@gmail.com' }
-member_1 = {'name': 'valentim', 'slackname': 'Jose Valentim', 'email':'jose.valentimfilhoUK@gmail.com' }
-member_2 = {'name': 'valentim', 'slackname': 'Jose Valentim', 'email':'jose.valentimfilhoUK@gmail.com' }
-member_3 = {'name': 'valentim', 'slackname': 'Jose Valentim', 'email':'jose.valentimfilhoUK@gmail.com' }
-member_4 = {'name': 'valentim', 'slackname': 'Jose Valentim', 'email':'jose.valentimfilhoUK@gmail.com' }
+member_0 = {'name': 'valentim', 'slackname': 'Jose Valentim', 'email':'jose.valentimfilhoUK@gmail.com', 'country' : 'BR', 'Hobbies': 'Football', 'Discipline': 'Artificial Inteligence', 'Language': 'Portuguese' }
+member_1 = {'name': 'Mikayla Sheild', 'slackname': 'Mikayla', 'email':'msheild.1218@gmail.com', 'country' : 'UK', 'Hobbies': 'Reading', 'Discipline': 'Artificial Inteligence', 'Language': 'English' }
+member_2 = {'name': 'Josiah', 'slackname': 'Josiah', 'email':'jose.valentimfilhoUK@gmail.com','country' : 'UK', 'Hobbies': 'Reading', 'Discipline': 'Artificial Inteligence', 'Language': 'English' }
+member_3 = {'name': 'Happiness', 'slackname': 'Happiness', 'email':'ezinnahappiness11@gmail.com','country' : 'UK', 'Hobbies': 'Reading', 'Discipline': 'Artificial Inteligence', 'Language': 'English' }
+member_4 = {'name': 'Rashid', 'slackname': 'Rashid', 'email':'jose.valentimfilhoUK@gmail.com','country' : 'UK', 'Hobbies': 'Reading', 'Discipline': 'Artificial Inteligence', 'Language': 'English' }
 
 members = []
 members.append(member_0)
